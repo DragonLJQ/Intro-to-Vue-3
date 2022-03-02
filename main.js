dd74824d52070bd9c087d5680503aea8f668cfd4
DragonLJQ <jlongaf@connect.ust.hk>
2022-03-02 19:26:00 +0800

    L4 finish

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            instock: false,
+            inventory: 0,
+            onsale: true,
         }
     }
 })
