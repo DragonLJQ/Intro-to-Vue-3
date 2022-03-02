f3c62c2d791beafbfca1712cec6ba74bcf4b6dea
DragonLJQ <jlongaf@connect.ust.hk>
2022-03-02 17:02:44 +0800

    L2 finish

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Applex1',
+            descrption: 'This is some text for explain the product'
+        }
+    }
+})
