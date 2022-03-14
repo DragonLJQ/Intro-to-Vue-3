010fd0d69fbe0bdf0e7b5e53e91c623794057a8c
DragonLJQ <jlongaf@connect.ust.hk>
2022-03-14 15:43:58 +0800

    final finish

---
+++ components/ReviewForm.js
@@ -0,0 +1,55 @@
+app.component('review-form', {
+    template:
+    /*html*/
+    `<form class="review-form" @submit.prevent="onSubmit">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name">
+
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review"></textarea>
+
+    <label for="question">Question:</label>      
+    <textarea id="question" v-model="question"></textarea>
+
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+
+    <input class="button" type="submit" value="Submit">
+  </form>`,
+  data(){
+      return{
+          name: '',
+          review: '',
+          question: '',
+          rating: null
+      }
+  },
+  methods: {
+      onSubmit(){
+          if(this.name === '' || this.review === '' || this.rating === null
+          || this.question === ''){
+              alert("Not complete!")
+              return
+          }
+          let productReview = {
+              name: this.name,
+              review: this.review,
+              question: this.question,
+              rating: this.rating
+          }
+          this.$emit('review-submitted', productReview)
+
+          this.name = '',
+          this.review = '',
+          this.question = '',
+          this.rating = null
+      }
+  }
+})
